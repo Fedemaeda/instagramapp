25dbdff2fc68453ca3dbfa5da76b2c467d5ded59
Federico <fedemaeda@gmail.com>
2023-09-27 16:48:16 -0300

    boton 4

--- js/index.js
+++ js/index.js
@@ -2,4 +2,5 @@ const boton = document.querySelector(".boton");
 
 boton.addEventListener("click", function() {
   // Realiza una acción cuando el usuario hace clic en el botón
+  console.log("El botón ha sido clicado");
 });
