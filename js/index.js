fa732cd0027202b2ff85f32d5c84c135ead6eaa3
Federico <fedemaeda@gmail.com>
2023-09-27 16:53:48 -0300

    boton 7

--- js/index.js
+++ js/index.js
@@ -2,5 +2,5 @@ const boton = document.querySelector(".boton");
 
 boton.addEventListener("click", function() {
   // Realiza una acción cuando el usuario hace clic en el botón
-  console.log("El botón ha sido clicado");
+  window.open("https://www.instagram.com/accounts/login/?next=https://www.instagram.com/");
 });
